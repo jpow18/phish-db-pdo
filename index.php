d18f1fc49d6a88d779ed6795366da20be749a1be
James Pow <jbpow18@icloud.com>
2023-02-01 15:07:04 -0500

    Added meta tags and CSS link tag in head.

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ include_once("./config/Database.php");
 <html>
 
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Phish Show DB</title>
+  <link rel="stylesheet" href="./css/main.css">
 </head>
 
 <body>
