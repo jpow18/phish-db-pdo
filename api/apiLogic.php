8c7a26c1798d29a3780c1a5dfc5e73c30013884d
James Pow <jbpow18@icloud.com>
2023-02-01 20:29:37 -0500

    changed initialization of variables from Phish API to get the showdate.

--- api/apiLogic.php
+++ api/apiLogic.php
@@ -11,48 +11,48 @@
     // Define the API endpoint URL
     $url = "https://api.phish.net/v5/setlists/showdate/" . $showDate . ".json?apikey=1D0A891F8076BD89B4BD&order_by=showdate";
 
-  // Set the API key
-  $apiKey = "1D0A891F8076BD89B4BD";
+    // Set the API key
+    $apiKey = "1D0A891F8076BD89B4BD";
 
-  // Start a cURL session
-  $ch = curl_init();
+    // Start a cURL session
+    $ch = curl_init();
 
-  // Set cURL options to make a GET request to the API endpoint
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "X-API-Key: $apiKey"
-  ]);
+    // Set cURL options to make a GET request to the API endpoint
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      "X-API-Key: $apiKey"
+    ]);
 
-  // Execute the cURL request
-  $response = curl_exec($ch);
+    // Execute the cURL request
+    $response = curl_exec($ch);
 
-  // Check if there was an error with the cURL request
-  if (curl_errno($ch)) {
-    // Print the error message
-    echo "cURL error: " . curl_error($ch);
-    exit;
-  }
-
-  // Close the cURL session
-  curl_close($ch);
-
-  // Decode the JSON response into a PHP array
-  $data = json_decode($response, true);
-
-  //var_dump($data);
-
-  // Check if the response has any data
-  if (count($data["data"]) > 0) {
-    // Check if the decoded JSON response is not empty
-    if (!empty($data)) {
-      // Initialize variables for use in SQL insert statement
-        $country = [$data["data"][0]['country']];
-        $state = [$data["data"][0]['state']];
-        $city = [$data["data"][0]['city']];
-        $venue = [$data["data"][0]['venue']];
+    // Check if there was an error with the cURL request
+    if (curl_errno($ch)) {
+      // Print the error message
+      echo "cURL error: " . curl_error($ch);
+      exit;
     }
-  }
+
+    // Close the cURL session
+    curl_close($ch);
+
+    // Decode the JSON response into a PHP array
+    $data = json_decode($response, true);
+
+    //var_dump($data);
+
+    // Check if the response has any data
+    if (count($data["data"]) > 0) {
+      // Check if the decoded JSON response is not empty
+      if (!empty($data)) {
+        // Initialize variables for use in SQL insert statement
+          $date = $data["data"][0]['showdate'];
+          $state = $data["data"][0]['state'];
+          $city = $data["data"][0]['city'];
+          $venue = $data["data"][0]['venue'];
+      }
+    }
   }
 
 
