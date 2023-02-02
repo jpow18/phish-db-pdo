84fe2df5136add9947f58e7a80db4633b1383500
James Pow <jbpow18@icloud.com>
2023-02-01 20:28:42 -0500

    Added sql statements and included apiLogic.php

--- config/Database.php
+++ config/Database.php
@@ -1,5 +1,8 @@
 <?php
 
+  include_once("./api/apiLogic.php");
+
+
   $host = 'localhost';
   $dbName = 'phishdb';
   $user = 'root';
@@ -8,6 +11,12 @@
   try {
     $db = new PDO("mysql:host={$host};dbname={$dbName}", $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Insert information gathered from GET into phishdb using named parameters
+    $sql = 'INSERT INTO showinfo(date, state, city, venue) VALUES(:date, :state, :city, :venue)';
+    $stmt = $db->prepare($sql);
+    $stmt->execute(['date' => $date, 'state' => $state, 'city' => $city, 'venue' => $venue]);
+    
   } catch (PDOException $e) {
     $error_message = 'Database Error: ';
     $error_message .= $e->getMessage();
